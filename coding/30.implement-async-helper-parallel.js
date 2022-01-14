3dab435d31ef6622cd72986103a87fd2ba2ba89e
fenghan <fenghan770@gmail.com>
2022-01-14 14:41:05 +0800

    #30

---
+++ coding/30.implement-async-helper-parallel.js
@@ -0,0 +1,33 @@
+/*
+type Callback = (error: Error, data: any) => void
+
+type AsyncFunc = (
+   callback: Callback,
+   data: any
+) => void
+
+*/
+
+/**
+ * @param {AsyncFunc[]} funcs
+ * @return {(callback: Callback) => void}
+ */
+function parallel(funcs) {
+  return function (callback, initial) {
+    let i = 0
+    let error
+    const data = []
+
+    const cb = (err, v) => {
+      if (!error) {
+        err
+          ? callback((error = err))
+          : i++ === funcs.length - 1
+          ? callback(undefined, [...data, v])
+          : data.push(v)
+      }
+    }
+
+    funcs.forEach((fn) => fn(cb, initial))
+  }
+}
