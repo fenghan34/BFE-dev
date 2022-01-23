9deb653862a3ddac7c1b223d83ddb4b743d186bb
Feng <fenghan770@gmail.com>
2022-01-23 11:19:35 +0800

    #44

---
+++ coding/44.implement-Selection-Sort.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ */
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIndex = i
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j
+      }
+    }
+
+    if (minIndex === i) continue
+
+    const temp = arr[i]
+    arr[i] = arr[minIndex]
+    arr[minIndex] = temp
+  }
+}
