440374b328f2600067b484af2996ae27ac7eb67f
Feng Han <hi@fenghan.link>
2022-02-03 15:32:07 +0800

    Update #24

--- coding/create-a-priority-queue.js
+++ coding/create-a-priority-queue.js
@@ -1,50 +1,36 @@
 class PriorityQueue {
   /**
    * @param {(a: any, b: any) => -1 | 0 | 1} compare -
-   * compare function, similar to parameter of Array.prototype.sort
+   * compare function, similar to parameter of heap.prototype.sort
    */
   constructor(compare) {
     this.compare = compare
-    this.array = []
+    this.heap = []
   }
 
   /**
    * return {number} amount of items
    */
   size() {
-    return this.array.length
+    return this.heap.length
   }
 
   /**
    * returns the head element
    */
   peek() {
-    return this.array[0]
+    return this.heap[0]
   }
 
   /**
    * @param {any} element - new element to add
    */
   add(element) {
-    let start = 0
-    let end = this.array.length - 1
+    this.heap.push(element)
 
-    while (start <= end) {
-      const mid = (start + end) >> 1
-
-      const compareRes = this.compare(element, this.array[mid])
-
-      if (compareRes > 0) {
-        start = mid + 1
-      } else if (compareRes < 0) {
-        end = mid - 1
-      } else {
-        start = mid
-        break
-      }
+    if (this.heap.length > 0) {
+      this.siftUp(this.heap.length - 1)
     }
-
-    this.array.splice(start, 0, element)
   }
 
   /**
@@ -52,6 +38,55 @@ class PriorityQueue {
    * @return {any} the head element
    */
   poll() {
-    return this.array.shift()
+    if (this.heap.length < 3) {
+      return this.heap.shift()
+    }
+
+    this.swap(0, this.heap.length - 1)
+    const element = this.heap.pop()
+    this.siftDown(0)
+    return element
+  }
+
+  siftUp(index) {
+    if (index <= 0) return
+
+    const parent = index >> 1
+
+    if (this.compare(this.heap[index], this.heap[parent]) < 0) {
+      this.swap(index, parent)
+      this.siftUp(parent)
+    }
+  }
+
+  siftDown(index) {
+    let j = index
+    const left = index * 2 + 1
+    const right = index * 2 + 2
+
+    if (
+      left < this.heap.length &&
+      this.compare(this.heap[j], this.heap[left]) > 0
+    ) {
+      j = left
+    }
+
+    if (
+      right < this.heap.length &&
+      this.compare(this.heap[j], this.heap[right]) > 0
+    ) {
+      j = right
+    }
+
+    if (j !== index) {
+      this.swap(j, index)
+      this.siftDown(j)
+    }
+  }
+
+  swap(a, b) {
+    const temp = this.heap[a]
+    this.heap[a] = this.heap[b]
+    this.heap[b] = temp
   }
 }
