a48eba68587106082c5e2e8496d688abe2c73186
Feng Han <hi@fenghan.link>
2022-02-05 11:53:00 +0800

    #50

---
+++ coding/50.search-element-right-before-target-with-Binary-Search-possible-duplicate-array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} arr - ascending array with duplicates
+ * @param {number} target
+ * @return {number}
+ */
+function elementBefore(arr, target) {
+  let start = 0
+  let end = arr.length - 1
+
+  while (start <= end) {
+    let mid = (start + end) >> 1
+
+    if (arr[mid] > target) {
+      end = mid - 1
+    } else if (arr[mid] < target) {
+      start = mid + 1
+    } else {
+      while (arr[mid - 1] === arr[mid]) {
+        mid--
+      }
+
+      return arr[mid - 1]
+    }
+  }
+
+  return undefined
+}
